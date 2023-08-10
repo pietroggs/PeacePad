2b62eac00f0e08c18dff79247dd4b8790be68e7f
pietroggs <pietroggsilva@gmail.com>
2023-08-09 22:34:52 -0300

    Adding center text of resize + anim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,32 +3,13 @@
   import Card from "../lib/components/Card.svelte";
   import { setContext } from "svelte";
 
-  let userMouse = {
-    dragging: false,
-
-    pos: { x: 0, y: 0 },
-
-    handleMousemove(event) {
-      if (true) {
-        userMouse.pos.x = event.clientX;
-        userMouse.pos.y = event.clientY;
-        console.log(userMouse.pos);
-      }
-    },
-    
-  };
   setContext("mouse", userMouse);
-
-  function expandDrag() {
-    userMouse.dragging = !userMouse.dragging;
-    console.log("drag: " + userMouse.dragging);
-  }
 </script>
 
 <h1 class="text-4xl font-medium">Hello world!</h1>
 <main>
   <div class="wrapper p-2">
-    <Card on:drag={expandDrag} />
+    <Card />
     <Card />
   </div>
 </main>
